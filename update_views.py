4953165f00fdca478de7e97f5ab4a4b2f9acd689
xoptech <xopxtech@gmail.com>
2026-04-20 02:51:57 +0800

    feat(whoami): Stand proud. You're strong.

--- update_views.py
+++ update_views.py
@@ -1,8 +1,13 @@
-import os, requests, re
-
-r = requests.get(f'https://api.github.com/repos/{os.environ.get("GITHUB_REPOSITORY")}/traffic/views', headers={'Authorization': f'token {os.environ.get("PAT_TOKEN")}'})
-print('API response:', r.text)
-
+import os, requests, re, sys
+repo = os.environ.get('GITHUB_REPOSITORY')
+token = os.environ.get('PAT_TOKEN')
+r = requests.get(f'https://api.github.com/repos/{repo}/traffic/views', headers={'Authorization': f'token {token}'})
+print('status:', r.status_code)
+print('response:', r.text)
+if r.status_code != 200:
+    sys.exit(1)
 v = r.json().get('count', 0)
-c = open('README.md').read()
-open('README.md', 'w').write(re.sub(r'views: \d+', f'views: {v}', c))
+with open('README.md') as f:
+    c = f.read()
+with open('README.md', 'w') as f:
+    f.write(re.sub(r'views: \d+', f'views: {v}', c))
