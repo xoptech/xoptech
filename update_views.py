f2651fb110bef07de904d8ee15d46ae4d6fe3f1d
xoptech <xopxtech@gmail.com>
2026-04-20 02:30:29 +0800

    feat(whoami): Stand proud. You're strong.

--- update_views.py
+++ update_views.py
@@ -1,5 +1,5 @@
 import os, requests, re
 
-v = requests.get(f'https://api.github.com/repos/{os.environ.get("GITHUB_REPOSITORY")}/traffic/views', headers={'Authorization': f'token {os.environ.get("GITHUB_TOKEN")}'}).json().get('count', 0)
+v = requests.get(f'https://api.github.com/repos/{os.environ.get("GITHUB_REPOSITORY")}/traffic/views', headers={'Authorization': f'token {os.environ.get("PAT_TOKEN")}'}).json().get('count', 0)
 c = open('README.md').read()
 open('README.md', 'w').write(re.sub(r'views: \d+', f'views: {v}', c))
