74307465c6e2ff2765e0a0bae1ef232a4e29eddf
xoptech <xopxtech@gmail.com>
2026-04-20 03:27:08 +0800

    feat(whoami): Stand proud. You're strong.

--- update_views.py
+++ update_views.py
@@ -1,13 +1,11 @@
-import os, requests, re, sys
-repo = os.environ.get('GITHUB_REPOSITORY')
-token = os.environ.get('PAT_TOKEN')
-r = requests.get(f'https://api.github.com/repos/{repo}/traffic/views', headers={'Authorization': f'token {token}'})
-print('status:', r.status_code)
-print('response:', r.text)
-if r.status_code != 200:
-    sys.exit(1)
+import os, requests, re
+repo, token = os.environ['GITHUB_REPOSITORY'], os.environ['PAT_TOKEN']
+url = f'https://api.github.com/repos/{repo}/traffic/views'
+r = requests.get(url, headers={'Authorization': f'token {token}'})
 v = r.json().get('count', 0)
-with open('README.md') as f:
-    c = f.read()
-with open('README.md', 'w') as f:
-    f.write(re.sub(r'views: \d+', f'views: {v}', c))
+print(f'Fetched views: {v}')
+with open('README.md', 'r+') as f:
+    text = re.sub(r'views: \d+', f'views: {v}', f.read())
+    f.seek(0)
+    f.write(text)
+    f.truncate()
